34c6580bcb3e1039c522c8a3ea663f89f4ac76a1
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-08-15 19:54:44 +0300

    Add parameter to model

--- src/main/java/com/MyController.java
+++ src/main/java/com/MyController.java
@@ -1,7 +1,10 @@
 package com;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -16,8 +19,18 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmployeeDetails() {
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmployeeDetails() {
+    public String showEmployeeDetails(HttpServletRequest request, Model model) {
+        String name = request.getParameter("employeeName");
+        name = "Mr. " + name;
+        model.addAttribute("nameAttribute", name);
         return "show-emp-details-view";
     }
+
+
 }
--- src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
+++ src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
@@ -13,7 +13,8 @@
 <body>
     <h2>Dear Employee, you are WELCOME!</h2>
     <br><br>
-    <h3>Your name: ${param.employeeName}</h3>
+<%--    <h3>Your name: ${param.employeeName}</h3>--%>
+    <h3>Welcome ${nameAttribute}!</h3>
     <h3>Your age: ${param.employeeAge}</h3>
 
 </body>
